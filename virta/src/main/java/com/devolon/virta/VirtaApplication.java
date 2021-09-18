c936cf40e21dd54fb31659d7979524e8c001ceae
Morteza eslami <eslami.morteza86@gmail.com>
2021-09-18 19:01:14 +0430

    Initial multiple module project for virta

---
+++ virta/src/main/java/com/devolon/virta/VirtaApplication.java
@@ -0,0 +1,13 @@
+package com.devolon.virta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtaApplication.class, args);
+    }
+
+}
---
+++ virta/src/test/java/com/devolon/virta/VirtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devolon.virta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
