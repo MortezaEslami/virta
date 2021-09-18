15fee5954691503bd96bd815b48fa2a1d0904232
Morteza eslami <eslami.morteza86@gmail.com>
2021-09-18 19:12:43 +0430

    Add below entity class to project
     Company , Station

---
+++ virta/src/main/java/com/devolon/virta/model/Company.java
@@ -0,0 +1,35 @@
+package com.devolon.virta.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "tbl_company")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "C_NAME", nullable = false)
+    private String name;
+
+    @Column(name = "C_CODE", nullable = false)
+    private String code;
+
+    @Column(name = "C_COMMENT", nullable = false)
+    private String comment;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToOne
+    @JoinColumn(name = "N_PARENT_ID", insertable = false, updatable = false)
+    private Company parent;
+
+    @Column(name = "N_PARENT_ID")
+    private Long parentId;
+
+}
---
+++ virta/src/main/java/com/devolon/virta/model/Station.java
@@ -0,0 +1,41 @@
+package com.devolon.virta.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "tbl_station")
+public class Station {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "C_NAME", nullable = false)
+    private String name;
+
+    @Column(name = "C_CODE", nullable = false)
+    private String code;
+
+    @Column(name = "C_LATITUDE")
+    private Double latitude;
+
+    @Column(name = "C_LONGITUDE")
+    private Double longitude;
+
+    @Column(name = "C_COMMENT", nullable = false)
+    private String comment;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToOne
+    @JoinColumn(name = "N_COMPANY_ID", insertable = false, updatable = false)
+    private Company company;
+
+    @Column(name = "N_COMPANY_ID")
+    private Long companyId;
+
+}
